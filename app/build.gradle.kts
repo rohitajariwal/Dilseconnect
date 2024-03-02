85369e2f571decf3ef65424e7f468a79dfca3a48
rohitajariwal <rohitajariwal01@gmail.com>
2024-03-02 12:26:39 +0530

    Solved the gradle errors and launching the login page as the app starts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.dilseconnect"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.dilseconnect"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,6 +30,7 @@ android {
         }
     }
     compileOptions {
+
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -40,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
     packaging {
         resources {
@@ -51,13 +52,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -65,7 +66,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- app/src/main/java/com/example/dilseconnect/Login.kt
+++ app/src/main/java/com/example/dilseconnect/Login.kt
@@ -7,24 +7,25 @@ import android.widget.Button
 import android.widget.EditText
 
 class Login : AppCompatActivity() {
+    private  lateinit var kedt_email: EditText
+    private  lateinit var kedt_pass: EditText
+    private  lateinit var kbtn_login: Button
+    private  lateinit var kbtn_signup: Button
 
-    private lateinit var kedt_email:EditText
-    private lateinit var kedt_pass:EditText
-    private lateinit var kbtn_login:Button
-    private lateinit var kbtn_signup:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        kedt_email=findViewById(R.id.edt_email)
-        kedt_pass=findViewById(R.id.edt_pass)
-        kbtn_login=findViewById(R.id.btn_login)
-        kbtn_signup=findViewById(R.id.btn_signup)
+        kedt_email = findViewById(R.id.edt_email)
+        kedt_pass = findViewById(R.id.edt_pass)
+        kbtn_login = findViewById(R.id.btn_login)
+        kbtn_signup = findViewById(R.id.btn_signup)
 
         kbtn_signup.setOnClickListener {
-            val i=Intent(this, SignUp::class.java)
-            startActivity(i)
-        }
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
 
+
+        }
     }
 }
