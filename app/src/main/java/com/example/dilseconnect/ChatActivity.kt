3baf22f295240b1d7b2c2c05009502e07d3fb403
agarwalraksha <agarwalraksha@outlook.com>
2024-03-03 23:24:29 +0530

    initialized chat activity kt file and connected user adaptor to chat activity: raksha

--- app/src/main/java/com/example/dilseconnect/ChatActivity.kt
+++ app/src/main/java/com/example/dilseconnect/ChatActivity.kt
@@ -1,11 +1,28 @@
 package com.example.dilseconnect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var kchat_rv:RecyclerView
+    private lateinit var kmsg_box:EditText
+    private lateinit var ksend:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        val intent = Intent
+        val name = intent.getStringExtra("name")
+        val uid = intent.getStringExtra("uid")
+
+        supportActionBar?.title = name
+
+        kchat_rv = findViewById(R.id.chat_rv)
+        kmsg_box = findViewById(R.id.msg_box)
+        ksend = findViewById(R.id.send)
     }
 }
--- app/src/main/java/com/example/dilseconnect/UserAdaptor.kt
+++ app/src/main/java/com/example/dilseconnect/UserAdaptor.kt
@@ -1,11 +1,13 @@
 package com.example.dilseconnect
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import java.security.AccessControlContext
 
 class UserAdaptor (val context: Context, val userList: ArrayList<User>):
@@ -22,6 +24,14 @@ class UserAdaptor (val context: Context, val userList: ArrayList<User>):
         val currentUser = userList[position]
 
         holder.textName.text = currentUser.name
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context,ChatActivity::class.java)
+            intent.putExtra("name",currentUser.name)
+            intent.putExtra("uid",FirebaseAuth.getInstance().currentUser?.uid)
+
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
