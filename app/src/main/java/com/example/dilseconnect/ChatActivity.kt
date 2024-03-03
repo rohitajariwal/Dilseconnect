960b21662251a5373ef9cc602c8c02058cdb4ce0
rohitajariwal <rohitajariwal01@gmail.com>
2024-03-03 23:25:53 +0530

    Created MessageAdapter kt file.

--- app/src/main/java/com/example/dilseconnect/ChatActivity.kt
+++ app/src/main/java/com/example/dilseconnect/ChatActivity.kt
@@ -15,7 +15,7 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        val intent = Intent
+        val intent = Intent()
         val name = intent.getStringExtra("name")
         val uid = intent.getStringExtra("uid")
 
