037aa010545d25e7df1108fc64189afce38c810c
rohitajariwal <rohitajariwal01@gmail.com>
2024-03-03 22:48:25 +0530

    Created Message receiver and sending message kt files.

---
+++ app/src/main/java/com/example/dilseconnect/Message.kt
@@ -0,0 +1,14 @@
+package com.example.dilseconnect
+
+class Message {
+    var message: String? = null
+    var senderId:String? = null
+
+
+    constructor(){}
+
+    constructor(message: String?, senderId: String?){
+        this.message = message
+        this.senderId = senderId
+    }
+}
---
+++ app/src/main/java/com/example/dilseconnect/MessageAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.dilseconnect
+
+import androidx.recyclerview.widget.RecyclerView
+
+class MessageAdapter: RecyclerView.Adapter {
+
+
+}
