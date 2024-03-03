d028aa001493aab09b1bd08d4ddfa8b2a6f3332b
rohitajariwal <rohitajariwal01@gmail.com>
2024-03-03 18:31:11 +0530

    Started UserAdapter kt.

---
+++ app/src/main/java/com/example/dilseconnect/UserAdaptor.kt
@@ -0,0 +1,10 @@
+package com.example.dilseconnect
+
+import androidx.recyclerview.widget.RecyclerView
+
+class UserAdaptor: RecyclerView.Adapter {
+
+    class UserViewHolder: RecyclerView.ViewHolder{
+
+    }
+}
