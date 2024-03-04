b5ecd6253f55ba2d9a9a1b3e4cfbc9553ef2afaa
rohitajariwal <rohitajariwal01@gmail.com>
2024-03-04 12:09:01 +0530

    Added Sender and Receiver Room in the chat activity.

--- app/src/main/java/com/example/dilseconnect/ChatActivity.kt
+++ app/src/main/java/com/example/dilseconnect/ChatActivity.kt
@@ -6,22 +6,53 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var kchat_rv:RecyclerView
     private lateinit var kmsg_box:EditText
     private lateinit var ksend:ImageView
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
+    private lateinit var mDbRef: DatabaseReference
+
+    var receiverRoom: String? = null
+    var senderRoom: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
         val name = intent.getStringExtra("name")
-        val uid = intent.getStringExtra("uid")
+        val receiverUid = intent.getStringExtra("uid")
+
+        val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        senderRoom = receiverUid + senderUid
+        receiverRoom = senderUid + receiverUid
 
         supportActionBar?.title = name
 
         kchat_rv = findViewById(R.id.chat_rv)
         kmsg_box = findViewById(R.id.msg_box)
         ksend = findViewById(R.id.send)
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this, messageList)
+
+        ksend.setOnClickListener{
+
+            val message = kmsg_box.text.toString()
+            val messageObject = Message(message,senderUid)
+
+            mDbRef.child("chats").child(senderRoom!!).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(receiverRoom!!).child("messages").push()
+                        .setValue(messageObject)
+                }
+
+
+        }
     }
 }
